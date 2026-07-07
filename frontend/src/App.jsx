a2a917f686d1418bc8073d859195cb2d25b055f4
ms4411 <myselftry0o0@gmail.com>
2026-07-07 19:17:07 +0900

    chore : 프론트앤드 첫 생성

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      안녕하세요
+    </div>
+  )
+}
+
+export default App
