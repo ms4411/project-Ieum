2eefedcaa71810716c73cfaa5435d44a68f9be30
ms4411 <myselftry0o0@gmail.com>
2026-07-08 20:09:13 +0900

    바텀 시트 작성 완료

--- frontend/src/component/BottomSheet.jsx
+++ frontend/src/component/BottomSheet.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react'
 import '../componentCss/BottomSheet.css'
+import { transform } from 'zod';
+import { group } from 'node:console';
 
 function BottomSheet(){
+    const [isSheetUp,setIsSheetUp]=useState(false);
+    const [groups, setGroups]=useState([]);
+    let MoveSheet=()=>{
+        if(isSheetUp)
+            setIsSheetUp(false)
+        else
+            setIsSheetUp(true)
+    }
+    let sheetCss={
+        transform:isSheetUp?'translateY(0)' : 'translateY(calc(100% - 50px))'
+    };
+
+    
     return(
         <>
-            <div id="bottom-sheet">
+            <div id="bottom-sheet" onClick={MoveSheet} style={sheetCss}>
                 <hr />
-                예시입니다
+                <ul id='group-list'>
+                    {groups.map((item, index) => (
+                    <Group key={index} content={item} /> // 배열을 순회하며 동적 요소 렌더링
+                    ))}
+                </ul>
             </div>
         </>
     )
