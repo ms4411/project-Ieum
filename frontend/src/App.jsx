f69f6de778b1362980a6ab13c72ab3da17bbd611
ms4411 <myselftry0o0@gmail.com>
2026-07-09 12:23:23 +0900

    마커 겹침현상 해결

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import Menubar from './component/Menubar.jsx'
 import KakaoMap from './component/KakaoMap.jsx'
@@ -6,12 +7,14 @@ import BottomSheet from './component/BottomSheet.jsx';
 import BottomMenu from './component/BottomMenu.jsx';
 
 function App() {
-  const [mapObject,setMapObject] = useState(null);
-  const [currentMarker, setCurrentMarker] = useState(null);
+  const markerRef = useRef(null);
+  const setMarkerRef = (obj) => {markerRef.current=obj}
+  const [mapObject, setMapObject] = useState(null);
   const [groups, setGroups]=useState([]);
 
   // 버튼을 눌러서 언제든 내 위치로 다시 이동하고 싶을 때 쓰는 함수
   const handleMoveToCurrentLocation = () => {
+    console.log(markerRef);
     const { kakao } = window;
     if (navigator.geolocation && mapObject) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -22,8 +25,9 @@ function App() {
         // 부드럽게 내 위치로 중심 이동
         mapObject.panTo(locPosition);
         
-        if (currentMarker){
-          currentMarker.setMap(null);
+        if (markerRef.current && typeof markerRef.current.setMap === 'function') {
+          markerRef.current.setMap(null)
+          markerRef.current = null; // 지우고 나서 금고를 깨끗하게 비워줍니다.
         }
         const imageSrc = '../public/makerImg.png'; // 카카오 제공 내 위치 동그라미
         const imageSize = new kakao.maps.Size(24, 24); // 동그라미 크기
@@ -38,9 +42,9 @@ function App() {
 
         // 3. 🚨 생성한 마커를 화면에 보이는 지도 위에 올리기
         marker.setMap(mapObject);
+        setMarkerRef(marker);
         });
-        // 새 마커를 상태에 저장해 둡니다 (다음에 지우기 위해)
-        setCurrentMarker(marker);
+        
     }
   };
 
