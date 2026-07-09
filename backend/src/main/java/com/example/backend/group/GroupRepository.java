b53e148d936c62b0b17193b264bafe99f6664c93
ms4411 <myselftry0o0@gmail.com>
2026-07-10 08:48:18 +0900

    레포지토리 메소드 추가

--- backend/src/main/java/com/example/backend/group/GroupRepository.java
+++ backend/src/main/java/com/example/backend/group/GroupRepository.java
@@ -2,7 +2,9 @@ package com.example.backend.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GroupRepository extends JpaRepository<Group, UUID> {
+    Optional<Group> findByCreateUserId (UUID CreateUserId);
 }
