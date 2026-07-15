36d7b80d1235752324274f3d0df00d209202bd76
ms4411 <myselftry0o0@gmail.com>
2026-07-16 01:18:09 +0900

    모임장 기준 전체 조회 추가

--- backend/src/main/java/com/example/backend/domain/group/GroupRepository.java
+++ backend/src/main/java/com/example/backend/domain/group/GroupRepository.java
@@ -2,9 +2,11 @@ package com.example.backend.domain.group;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface GroupRepository extends JpaRepository<Group, UUID> {
     Optional<Group> findByCreateUserId (UUID CreateUserId);
+    List<Group> findAllByCreateUserId (UUID CreateUserId);
 }
